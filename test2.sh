fa6b7a22b6752bd4f0366962376fa62b687d1e80
Caroline Roig-Irwin <clr2176@columbia.edu>
2017-10-20 20:13:54 -0400

    changed port to 8090

--- test2.sh
+++ test2.sh
@@ -1,6 +1,6 @@
-curl 'localhost:8080/demo/all'
-curl 'localhost:8080/demo/add?name=First&email=someemail@someemailprovider.com'
-curl 'localhost:8080/demo/add?name=FirstLAst&email=someemail@someemailprovider.com'
+curl 'localhost:8090/demo/all'
+curl 'localhost:8090/demo/add?name=First&email=someemail@someemailprovider.com'
+curl 'localhost:8090/demo/add?name=FirstLAst&email=someemail@someemailprovider.com'
 
-curl 'localhost:8080/demo/all'
+curl 'localhost:8090/demo/all'
 
